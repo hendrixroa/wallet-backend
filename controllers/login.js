38d834ec730d2b010e9d7746457180cd36928273
Hendrix Roa <hroa@master.inmobilia.com>
2017-05-27 08:54:23 -0400

    endpoint for login user created

--- controllers/login.js
+++ controllers/login.js
@@ -1,13 +1,25 @@
 var connection = require('../db/db');
+var sha1 = require('sha1');
 
 module.exports = {
 	loginUser: function(req, res){
 		connection.query('select * from users', function(err, rows, fields){
-			connection.end();
 			if(err != null){
 				console.log(err);
+				connection.end();
 			}else{
-				console.log(rows);	
+				console.log(rows);
+				console.log(
+					rows.find(function(user){
+						return user.username == req.body.username && user.password == sha1(req.body.password);
+					})
+				);
+
+				var result = rows.find(function(user){
+								return user.username == req.body.username && user.password == sha1(req.body.password);
+							});
+
+				res.json({'id': result == undefined ? null : result.id});
 			}
 			
 		});
