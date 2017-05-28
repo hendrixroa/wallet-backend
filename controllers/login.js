624d92d8e5bc442156063657c65a75e7281d3811
Hendrix Roa <hroa@master.inmobilia.com>
2017-05-28 14:51:56 -0400

    enablin coors for request from backend

--- controllers/login.js
+++ controllers/login.js
@@ -3,12 +3,12 @@ var sha1 = require('sha1');
 
 module.exports = {
 	loginUser: function(req, res){
-		connection.query('select * from users where username = ? AND password = ?', [req.body.username, sha1(req.body.password)] ,function(err, rows, fields){
+		connection.query('select id,username,is_admin from users where username = ? AND password = ?', [req.body.username, sha1(req.body.password)] ,function(err, rows, fields){
 			if(err != null){
 				console.log(err);
 				connection.end();
 			}else{
-				res.json({'user': rows ? rows : null});
+				res.send({'user': rows.length > 0 ? rows[0] : ''});
 			}		
 		});
 	}
--- server.js
+++ server.js
@@ -16,6 +16,13 @@ var db = require('./db/db');
 	app.use(bodyParser.text());                                    
 	app.use(bodyParser.json({ type: 'application/json'}));
 
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
+
+
 	app.route('/login')
 		.post(login.loginUser);
 
