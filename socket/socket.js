8367f8afeffb666ab123c359753ce202354da103
Hendrix Roa <hroa@master.inmobilia.com>
2017-06-05 17:49:24 -0400

    retired console logs of  socket

--- socket/socket.js
+++ socket/socket.js
@@ -3,7 +3,6 @@ module.exports = {
         io.sockets.on('connection', function(socket) {
             console.log('A client connected');
             socket.on('message', function(message) {
-                console.log('Hi bro, a message received '+ message);
                 io.sockets.emit('broadcast', message);
             });
         });
